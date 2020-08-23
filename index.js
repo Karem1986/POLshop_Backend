f3be1ea4130dd00a9cd87ac379101a13a7f2510d
Karem1986 <karem.ortiz@outlook.com>
2020-08-23 11:29:27 +0200

    Cors implementation with apollo server corrected.

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./src/schema");
 const resolvers = require("./src/resolvers");
 const models = require("./models");
-const cors = require("cors");
+// const cors = require("cors");
+
+//express was used to test cors working with apollo server but the correct way is to use it as in server below:
+// const express = require("express");
+
+// const app = express();
+// app.use(cors);
 
 const server = new ApolloServer({
+  cors: {
+    origin: "*", // <- allow request from all domains
+    credentials: true,
+  },
   typeDefs,
   resolvers,
-  cors,
   context: { models },
 });
 
